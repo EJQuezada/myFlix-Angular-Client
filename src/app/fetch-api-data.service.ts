d886c296b3dade06f04a8dad39741a86705bbac6
EJQuezada <equezada@msn.com>
2023-09-14 11:38:49 +0200

    update apiUrl

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs/operators';
 
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://edgarmovieflix.netlify.app/';
+const apiUrl = 'https://github.com/EJQuezada/my_movie_api';
 
 @Injectable({
   providedIn: 'root'
